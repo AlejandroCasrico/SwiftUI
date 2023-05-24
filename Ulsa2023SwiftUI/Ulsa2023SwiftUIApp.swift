1d7c4572a16590e8f69bb27c0d42553c02f56e02
AlejandriCasrico <casiricoalex@outlook.com>
2023-05-23 18:19:58 -0600

    Initial Commit

---
+++ Ulsa2023SwiftUI/Ulsa2023SwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  Ulsa2023SwiftUIApp.swift
+//  Ulsa2023SwiftUI
+//
+//  Created by Alejandro Casillas on 23/05/23.
+//
+
+import SwiftUI
+
+@main
+struct Ulsa2023SwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
